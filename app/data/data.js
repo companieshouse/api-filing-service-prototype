3aca34b1c3e4fdc15424746a2ea3e94a2d6266e9
Michael Gearon <mgearon@companieshouse.gov.uk>
2019-08-05 13:55:36 +0100

    Updates 2

--- app/data/data.js
+++ app/data/data.js
@@ -2,40 +2,56 @@ module.exports = [
   {
     id: 0,
     app: 'Application 1',
-    created: '30 July 2019',
-    test: '000000000',
-    live: '111111111',
-    future: '22222222',
+    appcreated: '30 July 2019',
+    apis: {
+      type: 'Rest API key',
+      created: '30 July 2019',
+      test: '000000000',
+      live: '111111111',
+      future: '22222222'
+    },
     ip: '194.75.36.81',
     javascript: ''
   },
   {
     id: 1,
     app: 'Application 2',
-    created: '30 July 2019',
-    test: '000000000',
-    live: '111111111',
-    future: '22222222',
+    appcreated: '30 July 2019',
+    apis: {
+      type: 'Rest API key',
+      created: '30 July 2019',
+      test: '000000000',
+      live: '111111111',
+      future: '22222222'
+    },
     ip: '194.75.36.81',
     javascript: ''
   },
   {
     id: 2,
     app: 'Application 3',
-    created: '30 July 2019',
-    test: '000000000',
-    live: '111111111',
-    future: '22222222',
+    appcreated: '30 July 2019',
+    apis: {
+      type: 'Rest API key',
+      created: '30 July 2019',
+      test: '000000000',
+      live: '111111111',
+      future: '22222222'
+    },
     ip: '194.75.36.81',
     javascript: ''
   },
   {
     id: 3,
     app: 'Application 4',
-    created: '30 July 2019',
-    test: '000000000',
-    live: '111111111',
-    future: '22222222',
+    appcreated: '30 July 2019',
+    apis: {
+      type: 'Rest API key',
+      created: '30 July 2019',
+      test: '000000000',
+      live: '111111111',
+      future: '22222222'
+    },
     ip: '194.75.36.81',
     javascript: ''
   }
