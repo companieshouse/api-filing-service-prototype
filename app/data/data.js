03a1c3a5d6ca44fe6c0512ce65d15b05e2347680
Michael Gearon <mgearon@companieshouse.gov.uk>
2019-08-12 11:41:05 +0100

    Changing the data

--- app/data/data.js
+++ app/data/data.js
@@ -1,56 +1,60 @@
 module.exports = [
   {
     id: 0,
-    app: 'Application 1',
-    appcreated: '30 July 2019',
+    app: 'My test application',
+    status: 'test',
+    appcreated: '22 July 2018',
     apis: {
       type: 'Rest API key',
       created: '30 July 2019',
-      test: '000000000',
-      live: '111111111',
-      future: '22222222'
+      test: 'ff78ff7b-542c-4a2c-85ba-c8ebe26ad23f',
+      live: '441d00a6-4d8d-48d2-bd24-c33a856111d9',
+      future: '19adffb4-ebc0-4134-9f85-40f48068f122'
     },
     ip: '194.75.36.81',
     javascript: ''
   },
   {
     id: 1,
-    app: 'Application 2',
-    appcreated: '30 July 2019',
+    app: 'Live service',
+    status: 'live',
+    appcreated: '12 January 2016',
     apis: {
       type: 'Rest API key',
       created: '30 July 2019',
-      test: '000000000',
-      live: '111111111',
-      future: '22222222'
+      test: '2efb02c5-ad72-46fd-b97c-1374d394b5af',
+      live: '583cbc2c-920c-4cb2-8f38-3fdd6721e3ce',
+      future: '2ab8fe3f-680b-4ea3-b707-4d8978e5d62d'
     },
     ip: '194.75.36.81',
     javascript: ''
   },
   {
     id: 2,
-    app: 'Application 3',
+    app: 'Future testing',
+    status: 'future',
     appcreated: '30 July 2019',
     apis: {
       type: 'Rest API key',
       created: '30 July 2019',
-      test: '000000000',
-      live: '111111111',
-      future: '22222222'
+      test: '7b20b648-5059-41b8-ab8f-0c4d48d83592',
+      live: '1e5e7cd8-b233-43a2-8ea2-f94ccecf29cd',
+      future: '3cf128f7-6a3d-4e94-b68b-9428bd6cc23a'
     },
     ip: '194.75.36.81',
     javascript: ''
   },
   {
     id: 3,
-    app: 'Application 4',
-    appcreated: '30 July 2019',
+    app: 'In development service',
+    status: 'test',
+    appcreated: '1 August 2019',
     apis: {
       type: 'Rest API key',
       created: '30 July 2019',
-      test: '000000000',
-      live: '111111111',
-      future: '22222222'
+      test: '70553427-1842-470c-a82a-f10f49bd54c6',
+      live: 'a3e57798-afdc-408d-8bc6-4bfc17825944',
+      future: 'd0210533-e268-4bbc-a754-fc8a7e60d3a3'
     },
     ip: '194.75.36.81',
     javascript: ''
