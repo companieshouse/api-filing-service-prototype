a824e495859afcad8a4398305040ffa753facce9
Michael Gearon <mgearon@companieshouse.gov.uk>
2020-06-15 08:37:49 +0100

    Adding dynamic email

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@ router.use(function (req, res, next) {
   res.locals.session = req.session
   next()
 })
-
+// Homepage
 router.get('/', function (req, res) {
   var realCases = require('./data/data.js')
   req.session.cases = realCases.concat()
@@ -16,23 +16,55 @@ router.get('/', function (req, res) {
   res.render('index')
   console.log(req.session.login)
 })
+// All pages
 router.all('*', function (req, res, next) {
   if (typeof req.session.cases === 'undefined') {
     return res.redirect('/')
   }
-  console.log(req.session.login)
   next()
 })
-router.post('/sign-in', function (req, res) {
-  req.session.login = true
 
-  res.redirect('manage-applications')
+// Sign in
+router.post('/sign-in', function (req, res) {
+  var errors = []
+  var emailHasError = false
+  var passwordHasError = false
+
+  if (req.session.data['email'] === '') {
+    emailHasError = true
+    errors.push({
+      text: 'Enter your email address',
+      href: '#email-error'
+    })
+  }
+  if (req.session.data['password'] === '') {
+    passwordHasError = true
+    errors.push({
+      text: 'Enter your password',
+      href: '#password-error'
+    })
+  }
+
+  if (emailHasError || passwordHasError) {
+    res.render('sign-in', {
+      errorEmail: emailHasError,
+      errorPassword: passwordHasError,
+      errorList: errors
+    })
+  } else {
+    req.session.login = true
+    res.redirect('manage-applications')
+  }
 })
+
+// Manage applications
 router.get('/manage-applications', function (req, res) {
   res.render('manage-applications', {
     cases: req.session.cases
   })
 })
+
+// View single application
 router.get('/view-application', function (req, res) {
   var id = parseInt(req.query.id)
   var i = 0
@@ -46,17 +78,22 @@ router.get('/view-application', function (req, res) {
 
   })
 })
+
+// Edit application
 router.get('/edit', function (req, res) {
   var id = parseInt(req.query.id)
   res.render('edit', {
     case: req.session.cases[id]
   })
 })
+
+// Register application
 router.get('/register', function (req, res) {
   res.render('register', {
   })
 })
 
+// Signout
 router.get('/signout', function (req, res) {
   req.session.login = false
   res.redirect('index')
